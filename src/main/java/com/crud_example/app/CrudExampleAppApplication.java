7667d49a42be0ff0242d3e244947ccb65ba3975f
josegassull <josegassull@gmail.com>
2024-12-06 15:52:51 -0300

    First commit

---
+++ src/main/java/com/crud_example/app/CrudExampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.crud_example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudExampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudExampleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud_example/app/CrudExampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud_example.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudExampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
